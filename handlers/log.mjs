c20a20153609789cd52f77b49da46d03297d543a
Jim Cronqvist <jim.cronqvist@gmail.com>
2025-11-27 19:39:57 +0100

    Improvements to logging

--- handlers/log.mjs
+++ handlers/log.mjs
@@ -7,11 +7,14 @@ const LOG_REQUEST  = env('LOG_REQUEST', false);
 const LOG_RESPONSE_BODY = env('LOG_RESPONSE_BODY', false);
 const LOG_REQUEST_BODY  = env('LOG_REQUEST_BODY', false);
 const LOG_IP = env('LOG_IP', true);
+const LOG_SINGLE_ROW = env('LOG_SINGLE_ROW', true);
 
 export const onRequest = !LOG_REQUEST ? undefined : async (req, res) => {
+  if(LOG_SINGLE_ROW) return;
+
   const ip = LOG_IP ? ` [${realClientIP(req)}]` : '';
   req.log.info(`Request:  ${req.method} ${req.url}${ip}`);
-  
+
   if(!LOG_REQUEST_BODY) return;
 
   if(req.is('application/json') || req.is('application/x-www-form-urlencoded')) {
@@ -23,20 +26,40 @@ export const onRequest = !LOG_REQUEST ? undefined : async (req, res) => {
 
 export const onResponse = !LOG_RESPONSE ? undefined : async (req, res, payload, proxyRes) => {
   const ip = LOG_IP ? ` [${realClientIP(req)}]` : '';
-  req.log.info({ res }, `Response: ${req.method} ${req.url} (${proxyRes.statusCode}) [${res.getHeader('X-Http-Proxy-Mode') || 'unknown'}] ${res.duration}ms${ip}`);
+  let msg = `Response: ${req.method} ${req.url} (${proxyRes.statusCode}) [${res.getHeader('X-Http-Proxy-Mode') || 'unknown'}] ${res.duration}ms${ip}`;
+  if(!LOG_SINGLE_ROW) {
+    req.log.info({ res }, msg);
+  }
 
-  if(!LOG_RESPONSE_BODY) return;
-
-  const contentType = (proxyRes.headers['content-type'] || '').split(';')[0].trim();
-  if(contentType === 'application/json') {
-    try {
-      const data = JSON.parse(payload);
-      req.log.info({ res }, "Response body:\n"+JSON.stringify(data, null, 2));
-      return JSON.stringify(data);
-    } catch(err) {
-      req.log.warn({ res, err }, 'Failed to parse JSON response payload; returning raw payload');
-      req.log.info({ res }, "Response body:\n"+payload);
-      return payload;
+  let responsePayload = null;
+  if(LOG_RESPONSE_BODY) {
+    const contentType = (proxyRes.headers['content-type'] || '').split(';')[0].trim();
+    if(contentType === 'application/json') {
+      try {
+        responsePayload = JSON.parse(payload);
+      } catch(err) {
+        req.log.warn({ res, err }, 'Failed to parse JSON response payload.');
+        req.log.info({ res }, "Response body:\n"+payload);
+        return;
+      }
+      if(!LOG_SINGLE_ROW) {
+        req.log.info({ res }, "Response body:\n"+JSON.stringify(responsePayload, null, 2));
+      }
     }
+  }
+
+  if(LOG_SINGLE_ROW) {
+    const requestPayload = req.is('application/json') || req.is('application/x-www-form-urlencoded') || req.is('text/plain') ? req.body : null;
+
+    if(msg.startsWith('Response: ')) {
+      msg = msg.slice('Response: '.length);
+    }
+    if(LOG_REQUEST_BODY && requestPayload) {
+      msg += `\nRequest body:\n${JSON.stringify(requestPayload, null, 2)}`;
+    }
+    if(LOG_RESPONSE_BODY && responsePayload) {
+      msg += `\nResponse body:\n${JSON.stringify(responsePayload, null, 2)}`;
+    }
+    req.log.info({ res }, msg);
   }
 };
